e207c33de98e1444fb500f8099ed6bf401192666
root <root@DESKTOP-E8CT236>
2024-08-15 13:26:57 +0300

    added module00, exercise00

---
+++ module00/ex00/megaphone.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cctype>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		(void)argv;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	}
+	else
+	{
+		for(int i = 1; i < argc; ++i)
+		{
+			for(int j = 0; argv[i][j]; ++j)
+				argv[i][j] = std::toupper(static_cast<unsigned char>(argv[i][j])); 
+		}
+		for(int i = 1; argv[i]; ++i)
+			std::cout << argv[i];
+		std::cout << std::endl;
+
+	}
+	return 0;
+}
