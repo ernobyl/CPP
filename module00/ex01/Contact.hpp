057a8e1914a241f3fe4f826284f471364f766b30
Erno Michelsson <emichels@c3r1p6.hive.fi>
2024-08-16 11:36:43 +0300

    declaring classes

---
+++ module00/ex01/Contact.hpp
@@ -0,0 +1,26 @@
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+# include <string>
+
+class	Contact
+{
+	private:
+		std::string	firstName;
+		std::string	lastName;
+		std::string	nickName;
+		std::string	phoneNumber;
+		std::string	darkSecret;
+	
+	public:
+		Contact();
+		void setContact(const std::string &fName, const std::string &lName, const std::string &nName, const std::string &pNumber, const std::string &dSecret);
+		void displayContact() const;
+		std::string getFirstName() const;
+		std::string getLastName() const;
+		std::string getNickName() const;
+		std::string getPhoneNumber() const;
+		std::string getDarkSecret() const;
+};
+
+#endif
---
+++ module00/ex01/PhoneBook.hpp
@@ -0,0 +1,19 @@
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+# include "Contact.hpp"
+
+class	PhoneBook
+{
+	private:
+		Contact	contacts[8];
+		int		oldestContactIndex;
+		int		contactCount;
+	
+	public:
+		PhoneBook();
+		void addContact(const Contact &newContact);
+		void displayContacts() const;
+};
+
+#endif
