dd7cb2652ec3fd04fa3b0ac68a94e0fcc9046588
Erno Michelsson <emichels@c3r3p6.hive.fi>
2024-09-10 10:18:08 +0300

    module02 ex01 ok

--- module02/ex01/Fixed.cpp
+++ module02/ex01/Fixed.cpp
@@ -1,9 +1,23 @@
 #include "Fixed.hpp"
-#include <iostream>
+#include <cmath>
+
+const int Fixed::_fractionalBits = 8;
 
 Fixed::Fixed() : _fixedPointValue(0)
 {
     std::cout << "Default constructor called." << std::endl;
+}
+
+Fixed::Fixed(const int param)
+{
+    std::cout << "Int constructor called." << std::endl;
+    _fixedPointValue = param << _fractionalBits;
+}
+
+Fixed::Fixed(const float param)
+{
+    std::cout << "Float constructor called." << std::endl;
+    _fixedPointValue = roundf(param * (1 << _fractionalBits));
 }
 
 Fixed::Fixed(const Fixed &other) : _fixedPointValue(other._fixedPointValue)
@@ -34,4 +48,20 @@ void Fixed::setRawBits(int const raw)
 {
     std::cout << "setRawBits member function called" << std::endl;
     this->_fixedPointValue = raw;
+}
+
+int Fixed::toInt(void) const
+{
+    return _fixedPointValue >> _fractionalBits;
+}
+
+float Fixed::toFloat(void) const
+{
+    return static_cast<float>(_fixedPointValue) / (1 << _fractionalBits);
+}
+
+std::ostream &operator<<(std::ostream &os, const Fixed &fixed)
+{
+    os << fixed.toFloat();
+    return (os);
 }
--- module02/ex01/Fixed.hpp
+++ module02/ex01/Fixed.hpp
@@ -1,6 +1,8 @@
 #ifndef FIXED_HPP
 #define FIXED_HPP
 
+#include <iostream>
+
 class Fixed
 {
     private:
@@ -8,11 +10,19 @@ class Fixed
         static const int _fractionalBits;
     public:
         Fixed();
+        Fixed(const int param);
+        Fixed(const float param);
         Fixed(const Fixed &other);
         Fixed &operator=(const Fixed &other);
+
         ~Fixed();
-        int getRawBits(void) const;
-        void setRawBits(int const raw);
+
+        int		getRawBits(void) const;
+        void	setRawBits(int const raw);
+		int		toInt(void) const;
+        float	toFloat(void) const;
 };
+
+std::ostream &operator<<(std::ostream &os, const Fixed &fixed);
 
 #endif
