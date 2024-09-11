96b94575dbd03ecf74a76fb52139e4bebe574756
root <root@DESKTOP-E8CT236>
2024-09-11 18:07:25 +0300

    started on module02 ex03

---
+++ module02/ex03/Fixed.hpp
@@ -0,0 +1,61 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed
+{
+	private:
+		int _fixedPointValue;
+		static const int _fractionalBits;
+	public:
+		/* Constructors */
+		Fixed();
+		Fixed(const int param);
+		Fixed(const float param);
+		Fixed(const Fixed &other);
+
+		/* Destructor */
+		~Fixed();
+
+		/* Operator overload functions */
+		Fixed	&operator=(const Fixed &other);
+		bool	operator>(Fixed fixed) const;
+		bool	operator<(Fixed fixed) const;
+		bool	operator>=(Fixed fixed) const;
+		bool	operator<=(Fixed fixed) const;
+		bool	operator==(Fixed fixed) const;
+		bool	operator!=(Fixed fixed) const;
+
+		float	operator+(Fixed fixed) const;
+		float	operator-(Fixed fixed) const;
+		float	operator*(Fixed fixed) const;
+		float	operator/(Fixed fixed) const;
+
+		Fixed	operator++();
+		Fixed	operator--();
+		Fixed	operator++(int);
+		Fixed	operator--(int);
+
+		/* Find min functions */
+		static			Fixed &min(Fixed &a, Fixed &b);
+		static const	Fixed &min(const Fixed &a, const Fixed &b);
+
+		/* Find max functions */
+		static			Fixed &max(Fixed &a, Fixed &b);
+		static const	Fixed &max(const Fixed &a, const Fixed &b);
+
+		/* Getter function */
+		int		getRawBits(void) const;
+
+		/* Setter function */
+		void	setRawBits(int const raw);
+
+		/* Converter functions */
+		int		toInt(void) const;
+		float	toFloat(void) const;
+};
+
+std::ostream &operator<<(std::ostream &os, const Fixed &fixed);
+
+#endif
