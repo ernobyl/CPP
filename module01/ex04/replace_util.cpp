da5eaf700e00e6a0067fb9e3eee76fe3f2b1bcbe
Erno Michelsson <emichels@c3r1p7.hive.fi>
2024-09-12 13:46:04 +0300

    modified module01 ex04

--- module01/ex04/replace_util.cpp
+++ module01/ex04/replace_util.cpp
@@ -8,7 +8,8 @@ std::string replaceOccurrences(const std::string& content, const std::string& s1
     size_t lastPos = 0;
     size_t s1Len = s1.length();
 
-    while ((pos = content.find(s1, lastPos)) != std::string::npos) {
+    while ((pos = content.find(s1, lastPos)) != std::string::npos)
+    {
         result.append(content, lastPos, pos - lastPos);
         result.append(s2);
         lastPos = pos + s1Len;
