59ae2709b019fc0cb957ed50c88f666502896969
Erno Michelsson <emichels@c3r1p7.hive.fi>
2024-09-12 14:53:29 +0300

    working on module02 ex03

--- module02/ex03/bsp.cpp
+++ module02/ex03/bsp.cpp
@@ -1,2 +1,9 @@
+#include "Fixed.hpp"
 #include "Point.hpp"
 
+Fixed area(Point const &p1, Point const &p2, Point const &p3)
+{
+    return Fixed ((p1.getX() * (p2.getY() - p3.getY()) +
+                    p2.getX() * (p3.getY() - p1.getY()) +
+                    p3.getX() * (p1.getY() - p2.getY())) / 2.0f);
+}
