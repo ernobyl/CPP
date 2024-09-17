887f1b3b946999084b8f9b7ad053c31b2126a932
Erno Michelsson <emichels@c1r4p6.hive.fi>
2024-09-17 12:16:10 +0300

    started on module03 ex00

---
+++ module03/ex00/ClapTrap.cpp
@@ -0,0 +1,6 @@
+#include "ClapTrap.hpp"
+
+ClapTrap(std::string _name)
+{
+    
+}
---
+++ module03/ex00/ClapTrap.hpp
@@ -0,0 +1,32 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <string>
+
+class ClapTrap
+{
+    private:
+        std::string _name;
+        int         _hitPoints;
+        int         _energyPoints;
+        int         _attackDamage;
+    public:
+        // Default constructor
+        ClapTrap(std::string _name);
+
+        // Copy constructor
+        ClapTrap(const ClapTrap& other);
+
+        // Copy assignment operator
+        ClapTrap& operator=(const ClapTrap& other);
+
+        // Destructor
+        ~ClapTrap();
+
+        void attack(const std::string& target);
+        void takeDamage(unsigned int amount);
+        void beRepaired(unsigned int amount);
+
+};
+
+#endif
