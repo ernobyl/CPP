2df1035cb8ff911853f0fe50c791cdcf1fbd2de9
Erno Michelsson <emichels@c1r1p5.hive.fi>
2025-01-17 10:59:45 +0200

    working on module06 ex01

---
+++ module06/ex01/main.cpp
@@ -0,0 +1,20 @@
+#include "Data.hpp"
+#include "Serializer.hpp"
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	Data		data{100, "Hello!"};
+	Data*		data_ptr;
+	uintptr_t	ptr;
+
+	std::cout << data.i << std::endl;;
+	std::cout << data.str << std::endl;;
+
+	ptr = Serializer::serialize(&data);
+	data_ptr = Serializer::deserialize(ptr);
+
+	std::cout << &data << std::endl;
+	std::cout << data_ptr << std::endl;
+
+}
