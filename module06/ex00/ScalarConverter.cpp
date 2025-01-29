3a083797a074eeb5dc3c1bbb783eb0fd730c9706
ernobyl <erno_m@hotmail.com>
2025-01-29 13:12:31 +0200

    module06 ex00: some rounding issues in floating point values, otherwise works

--- module06/ex00/ScalarConverter.cpp
+++ module06/ex00/ScalarConverter.cpp
@@ -54,7 +54,7 @@ void ScalarConverter::convertIntLiteral(const std::string& literal)
   	}
   	catch (std::out_of_range& e)
 	{
-    	convertFloatLiteral(literal);
+		convertDoubleLiteral(literal);
   	}
 }
 
@@ -63,7 +63,7 @@ void ScalarConverter::convertFloatLiteral(const std::string& literal)
 	try
 	{
 		f = std::stof(literal);
-		d = f;
+		d = std::stod(literal);
 		if ((d > 0.0 && d - std::numeric_limits<int>::max() >= 1.0) ||
 			(d < 0.0 && d - std::numeric_limits<int>::min() <= -1.0))
 		{
@@ -79,7 +79,7 @@ void ScalarConverter::convertFloatLiteral(const std::string& literal)
   }
   	catch (std::out_of_range& e)
 	{
-    	convertDoubleLiteral(literal);
+		convertDoubleLiteral(literal);
   	}
 }
 
@@ -88,35 +88,36 @@ void ScalarConverter::convertDoubleLiteral(const std::string& literal)
 	try
 	{
 		d = std::stod(literal);
-		if (!std::isinf(d) &&
-			((d > 0.0 && d - std::numeric_limits<float>::max() >= 1.0) ||
-			(d < 0.0 && d - std::numeric_limits<float>::lowest() <= -1.0)))
+
+		if (d > std::numeric_limits<float>::max() || d < -std::numeric_limits<float>::max())
 		{
-			c = INVALID_CHAR;
-			i = INVALID_INT;
-			f = std::numeric_limits<float>::max();
-   		}
-		else if ((d > 0.0f && d - std::numeric_limits<int>::max() >= 1.0) ||
-               (d < 0.0f && d - std::numeric_limits<int>::min() <= -1.0))
+			f = std::numeric_limits<float>::quiet_NaN();
+		}
+		else
 		{
-			c = INVALID_CHAR;
-			i = INVALID_INT;
 			f = static_cast<float>(d);
-    	}
+			if (f != d)
+				f = std::stof(literal);
+		}
+
+		if ((d > std::numeric_limits<int>::max()) || (d < std::numeric_limits<int>::min()) || std::isnan(d))
+		{
+			c = INVALID_CHAR;
+			i = INVALID_INT;
+		}
 		else
 		{
 			i = static_cast<int>(d);
-			f = static_cast<float>(d);
-			c = i >= 0 && i <= 127 ? static_cast<char>(i) : -1;
-    	}
-    displayResults();
-  }
-  catch (std::out_of_range& e)
-  {
-		std::cout << "char: impossible\n" << "int: impossible\n"
-				<< "float: impossible\n" << "double: impossible\n";
-  }
+			c = (i >= 0 && i <= 127) ? static_cast<char>(i) : -1;
+		}
+		displayResults();
+	}
+	catch (std::out_of_range& e)
+	{
+		std::cout << "char: impossible\nint: impossible\nfloat: impossible\ndouble: impossible\n";
+	}
 }
+
 
 void ScalarConverter::displayChar()
 {
@@ -125,29 +126,32 @@ void ScalarConverter::displayChar()
 	else
 	{
 		std::cout << "char: "
-              << (i >= 0 && i <= 127 ? "Non displayable" : "impossible") << '\n';
+			  << (i >= 0 && i <= 127 ? "Non displayable" : "impossible") << '\n';
 	}
 }
 
 void ScalarConverter::displayInt()
 {
 	if ((d > 0.0 && d - std::numeric_limits<int>::max() >= 1.0) ||
-    	(d < 0.0 && d - std::numeric_limits<int>::min() <= -1.0) ||
+		(d < 0.0 && d - std::numeric_limits<int>::min() <= -1.0) ||
 		(std::isnan(d)))
 		std::cout << "int: impossible\n";
 	else
-    	std::cout << "int: " << i << '\n';
+		std::cout << "int: " << i << '\n';
 }
 
 void ScalarConverter::displayFloat()
 {
-	if (!std::isinf(d) &&
-      ((d > 0.0 && d - std::numeric_limits<float>::max() >= 1.0) ||
-       (d < 0.0 && d - std::numeric_limits<float>::lowest() <= -1.0)))
-    	std::cout << "float: impossible\n";
-  	else
-    	std::cout << "float: " << std::fixed << std::setprecision(1) << f << "f\n";
+	if (std::isnan(f) || std::isinf(f))
+	{
+		std::cout << "float: impossible\n";
+	}
+	else
+	{
+		std::cout << "float: " << std::fixed << std::setprecision(1) << f << "f\n";
+	}
 }
+
 
 void ScalarConverter::displayDouble()
 {
